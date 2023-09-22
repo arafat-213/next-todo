997ed776ed929226f82b5effa8dff8776e2ccc05
Arafat Tai <tai.arafat.at@gmail.com>
2023-09-22 17:35:17 +0530

    add delete todo feature

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,19 +1,21 @@
-import { MinusCircleIcon } from '@heroicons/react/24/solid'
+import { useBoardStore } from '@/store/BoardStore'
+import { TrashIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 import { DraggableProvidedDragHandleProps, DraggableProvidedDraggableProps } from 'react-beautiful-dnd'
 
 type Props = {
     todo: Todo,
     index: number,
-    id: string,
+    id: TypedColumn,
     innerRef: (element: HTMLElement | null) => void
     draggableProps: DraggableProvidedDraggableProps
     dragHandleProps: DraggableProvidedDragHandleProps | null | undefined
 }
 function TodoCard({todo, index, id, innerRef, draggableProps, dragHandleProps}: Props) {
+  const [deleteTodo] = useBoardStore ((state) => [state.deleteTodo])
   return (
     <div
-    className='bg-white rounded-md my-2 drop-shadow-md space-y-2'
+    className='bg-white rounded-md my-2 drop-shadow-md space-y-2 group hover:bg-gray-100'
     {...draggableProps}
     {...dragHandleProps}
     ref= {innerRef}
@@ -22,8 +24,8 @@ function TodoCard({todo, index, id, innerRef, draggableProps, dragHandleProps}: 
         <p>
         {todo.title}
         </p>
-        <button className='text-red-500 hover:text-red-600'>
-            <MinusCircleIcon className='ml-5 h-8 w-8' />
+        <button onClick={() => deleteTodo(index, todo, id)} className='text-gray-300 hover:text-gray-400 opacity-0 group-hover:opacity-100'>
+            <TrashIcon className='ml-5 h-6 w-6' />
         </button>
      </div>
     </div>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn'
-import { databases } from '@/appwrite'
+import { databases, storage } from '@/appwrite'
 interface BoardState {
   board: Board
  getBoard: () => Promise<void>
@@ -10,12 +10,13 @@ interface BoardState {
  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => Promise <void>,
  searchString: string,
  setSearchString: (searchString: string) => void,
+ deleteTodo: (taskIndex: number, todo: Todo, id: TypedColumn) => Promise<void>,
 }
 
 export const useBoardStore = create<BoardState>()(
   devtools(
     persist(
-      (set) => ({
+      (set, get) => ({
         board: {
           columns: new Map<TypedColumn, Column>()
         },
@@ -35,6 +36,23 @@ export const useBoardStore = create<BoardState>()(
             }
           )
         },
+        deleteTodo: async (taskIndex, todo, id) => {
+          if (todo.image)
+            await storage.deleteFile(todo.image.bucketId, todo.image.fileId)
+          await databases.deleteDocument(
+            process.env.NEXT_PUBLIC_DATABASE_ID!,
+            process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+            todo.$id
+            )
+            const newColumns = new Map(get().board.columns)
+
+            // delete todoId from newColumns
+            newColumns.get(id)?.todos.splice(taskIndex, 1)
+            
+            set({ board: { columns: newColumns } })
+          },
+
+
         searchString: '',
         setSearchString: (searchString) => set({ searchString }),
       }),
